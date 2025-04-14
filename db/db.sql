2cbbb07109fc3bff58e344fa2061abefd078a0cd
Viraj Nikam <virajsnikam@gmail.com>
2025-04-14 08:40:36 +0530

    initial commit

---
+++ db/db.sql
@@ -0,0 +1,23 @@
+drop database if exit hackathon_db;
+
+create database hackathon_db;
+use hackathon_db;
+
+create table user(
+    id int auto_increment,
+    full_name varchar(50),
+    email varchar(50),
+    password varchar(50),
+    phone_no varchar(50),
+    created_time date(Date),
+    
+
+)
+
+
+
+
+
+
+
+
